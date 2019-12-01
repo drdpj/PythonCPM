0cc1a9e098924c2d89dbb0cc64b62d66242c9947
drdpj <djameson@gmail.com>
2019-12-01 08:08:03 +0000

    refactoring

--- readimd.py
+++ readimd.py
@@ -20,12 +20,12 @@ import struct
 sector_sizes = dict([ (0,128), (1,256), (2,512), (3,1024), (4,2048), \
     (5,4096), (6,8192) ])
 
-def FileCheck(fn):
+def file_check(fn):
     try:
         open (fn,"r")
         return 1
     except IOError:
-        print("\nNo such file.")
+        print("\nNo such file.\n")
         return 0
 
 
@@ -37,7 +37,7 @@ args=parser.parse_args()
 
 # Check the file exists
 file_name = args.filename[0]
-result = FileCheck(file_name)
+result = file_check(file_name)
 if result == 0 :
     exit()
 
@@ -55,54 +55,57 @@ while True:
 print(header)
 
 # Track at a time...
+# We assume that the file is the correct structure and we're
+# parsing everything that could possibly be in there...
+
 while True:
-    trackHeader = file.read(5)
-    if not trackHeader:
+    track_header = file.read(5)
+    if not track_header:
         break
     
-    trackMode = trackHeader[0]
-    trackCylinder = trackHeader[1]
-    trackHead = trackHeader[2]
-    trackSectors = trackHeader[3]
-    trackSectorSize = trackHeader[4]
+    track_mode = track_header[0]
+    track_cylinder = track_header[1]
+    track_head = track_header[2]
+    track_sectors = track_header[3]
+    track_sector_size = track_header[4]
 
-    trackSectorMap = file.read(trackSectors)
+    track_sector_map = file.read(track_sectors)
 
-    if trackHead & (1 << 7 ):
-        trackCylMap = file.read(trackSectors)
+    if track_head & (1 << 7 ):
+        track_cyl_map = file.read(track_sectors)
     else:
-        trackCylMap = None
+        track_cyl_map = None
     
-    if trackHead & (1 << 6):
-        trackHeadMap = file.read(trackSectors)
+    if track_head & (1 << 6):
+        track_head_map = file.read(track_sectors)
     else:
-        trackHeadMap = None
+        track_head_map = None
     
-    print("Track mode: ",trackMode)
-    print("Cylinder: ",trackCylinder)
-    print("Head: ",trackHead)
-    print("Sectors: ",trackSectors)
-    print("SectorSize: ",sector_sizes[trackSectorSize])
-    print("SectorMap: ",trackSectorMap)
-    if trackCylMap :
-        print ("CylinderMap: ",trackCylMap)
+    print("Track mode: ",track_mode)
+    print("Cylinder: ",track_cylinder)
+    print("Head: ",track_head)
+    print("Sectors: ",track_sectors)
+    print("SectorSize: ",sector_sizes[track_sector_size])
+    print("SectorMap: ",track_sector_map)
+    if track_cyl_map :
+        print ("Cylinder Map: ",track_cyl_map)
     
-    if trackHeadMap :
-        print ("HeadMap: ",trackHeadMap)
+    if track_head_map :
+        print ("Head Map: ",track_head_map)
 
     # for each sector
-    for i in range(0,trackSectors):
-        sectorStatus = file.read(1)[0]
-        if (sectorStatus ==  1) or  (sectorStatus ==  3) or (sectorStatus ==  5) or \
-            (sectorStatus ==  7):
-            sectorData = file.read(sector_sizes[trackSectorSize])
-        elif (sectorStatus ==  2) or  (sectorStatus ==  4) or (sectorStatus ==  6) or \
-            (sectorStatus ==  8):
-            sectorData = file.read(1)
+    for i in range(0,track_sectors):
+        sector_status_byte = file.read(1)[0]
+        if (sector_status_byte ==  1) or  (sector_status_byte ==  3) or (sector_status_byte ==  5) or \
+            (sector_status_byte ==  7):
+            sector_data = file.read(sector_sizes[track_sector_size])
+        elif (sector_status_byte ==  2) or  (sector_status_byte ==  4) or (sector_status_byte ==  6) or \
+            (sector_status_byte ==  8):
+            sector_data = file.read(1)
         else:
-            sectorData = None
-        print("Sector ",i," Status: ",sectorStatus)
-        print("Data:", sectorData)
+            sector_data = None
+        print("Sector ",i," Status: ",sector_status_byte)
+        print("Data:", sector_data)
 
 
 
